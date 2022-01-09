7de26fdd4724cd99cd5793abdc39c46f88d05698
vladmosessohn <vlad_mose@yahoo.com>
2022-01-09 19:50:55 +0200

    removed unique index in Expeditions table

---
+++ Migrations/Login/20220109174657_IndexRemoved.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourierBid.Migrations.Login
+{
+    public partial class IndexRemoved : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "IX_Expeditions_UserId", table: "Expeditions");
+            migrationBuilder.CreateIndex(name: "IX_Expeditions_UserId", table: "Expeditions", column: "UserId", unique: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
